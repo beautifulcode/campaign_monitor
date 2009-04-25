e654eac8793c6bfe4101f9808e5744b8407f0b3c
Aaron Glenn <aaron@aaron-glenns-macbook-pro.local>
2009-04-25 17:32:33 -0600

    Adding support for multi value custom fields.

--- lib/campaign_monitor/list.rb
+++ lib/campaign_monitor/list.rb
@@ -138,7 +138,13 @@ class CampaignMonitor
       def custom_fields_array(custom_fields)
         arr = []
         custom_fields.each do |key, value|
-          arr << { "Key" => key, "Value" => value }
+          if value.is_a? Array
+            value.each do |new_val|
+              arr << { "Key" => key, "Value" => new_val }
+            end
+          else
+            arr << { "Key" => key, "Value" => value }
+          end
         end
         arr
       end
